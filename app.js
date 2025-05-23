97659dc2c0768adcad0bdaca1296cac42415d363
YevheniiPavlov2006 <jackpavlov9@gmail.com>
2025-05-23 20:37:27 +0300

    public

---
+++ app.js
@@ -0,0 +1,20 @@
+new Swiper('.work-slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1050: {
+      slidesPerView: 3,
+    }
+  }
+})
