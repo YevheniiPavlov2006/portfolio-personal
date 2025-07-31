259b1ba283494c2949b4befd7a0c8f1ea44acc3e
YevheniiPavlov2006 <jackpavlov9@gmail.com>
2025-07-31 09:32:32 +0300

    merge

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const observer = new IntersectionObserver((entries) => {
     const ratio = entry.intersectionRatio;
 
     // Показываем, когда 30% видно
-    if (ratio >= 0.3) {
+    if (ratio >= 0.4) {
       entry.target.classList.add('show');
     }
 
@@ -36,9 +36,46 @@ const observer = new IntersectionObserver((entries) => {
     }
   });
 }, {
-  threshold: [0, 0.3]
+  threshold: [0, 0.4]
 });
 
 document.querySelectorAll('.animation-part').forEach(el => {
   observer.observe(el);
 });
+
+
+
+  const block = document.querySelector('.about-photos-block');
+  const lines = document.querySelectorAll('.about-photo-line');
+
+  const observer1 = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        lines.forEach(line => line.classList.add('animated'));
+      } else {
+        lines.forEach(line => line.classList.remove('animated'));
+      }
+    },
+    {
+      threshold: 0.4,
+    }
+  );
+
+  observer1.observe(block);
+
+  const info = document.querySelector('.about-info');
+
+  const observer2 = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        info.classList.add('animated')
+      } else {
+        info.classList.remove('animated')
+      }
+    },
+    {
+      threshold: 0.4,
+    }
+  );
+
+  observer2.observe(info);
